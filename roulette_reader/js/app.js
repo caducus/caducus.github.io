bc84709ce49379cd4fbf621b21e926572ec98d99
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-07-18 18:21:24 -0400

    completed css card styling

--- roulette_reader/js/app.js
+++ roulette_reader/js/app.js
@@ -11,6 +11,12 @@ $(() => {
   // Functions
   // ===========================
 
+  const showDescription = (event) => {
+    event.preventDefault();
+    $description = $(event.currentTarget).parent().siblings(".description");
+    $description.toggleClass("hidden");
+  };
+
   const generateList = (event) => {
     // prevent reloading
     event.preventDefault();
@@ -19,34 +25,54 @@ $(() => {
     // get the user's keyword from the input
     const keyword = $("input[type='text']").val();
 
-    // // connecting to the googlebooks api
-    // $.ajax({
-    //   url: "https://www.googleapis.com/books/v1/volumes?q=subject=" + keyword + "&key=" + apiKey
-    // }).then(
-    //   (data) => {
-    //     // loops through the 10 results
-    //     for (let i = 0; i < 10; i++) {
-    //       // create an empty card
-    //       $card = $("<div>");
-    //       // create each card element, adding the information from the googlebooks object
-    //       $title = $("<p>").text(data.items[i].volumeInfo.title || "No title is available.");
-    //       $author = $("<p>").text(data.items[i].volumeInfo.authors || "No author is available.");
-    //       $category = $("<p>").text(data.items[i].volumeInfo.categories || "No category is available.");
-    //       $descriptionDiv = $("<div>");
-    //       $description = $("<p>").text(data.items[i].volumeInfo.description || "No description is available.");
-    //       $image = $("<img>").attr("src", data.items[i].volumeInfo.imageLinks.thumbnail)
-    //       // append the created elements to the $card
-    //       $descriptionDiv.append($description);
-    //       $card.append($image).append($title).append($author).append($category).append($descriptionDiv);
-    //       // append the card to the page
-    //       $("#information").append($card);
-    //     };
-    //   },
-    //   () => {
-    //     // if something goes wrong:
-    //     console.log("Houston, we have a problem.");
-    //   }
-    // );
+    // connecting to the googlebooks api
+    $.ajax({
+      url: "https://www.googleapis.com/books/v1/volumes?q=" + keyword + "&key=" + apiKey
+    }).then(
+      (data) => {
+        // loops through the 10 results
+        for (let i = 0; i < 10; i++) {
+          // create an empty card
+          $card = $("<div>").addClass("card");
+          // create a div to hold an image
+          $imageDiv = $("<div>").addClass("imageDiv");
+          // create image
+          $image = $("<img>").attr("src", data.items[i].volumeInfo.imageLinks.thumbnail).addClass("bookThumbnail");
+          // create outerContainer div, which holds all other information
+          $outerContainer = $("<div>").addClass("outerContainer");
+          // create h2 title
+          $title = $("<h2>").text(data.items[i].volumeInfo.title || "No title is available.");
+          // create innerContainer div, which holds a div with two h3s, and a button
+          $innerContainer = $("<div>").addClass("innerContainer");
+          // create the div that holds the two h3s
+          $div = $("<div>")
+          // create the two h3 elements
+          $author = $("<h3>").text(data.items[i].volumeInfo.authors || "No author is available.");
+          $category = $("<h3>").text(data.items[i].volumeInfo.categories || "No category is available.");
+          // create the button
+          $button = $("<button>").text("show description");
+          // add event listener to button
+          $button.on("click", showDescription);
+          //create the div that holds the description
+          $descriptionDiv = $("<div>").addClass("description").addClass("hidden");
+          $description = $("<p>").text(data.items[i].volumeInfo.description || "No description is available.");
+          // append the elements to respective parents
+          $outerContainer.append($title).append($innerContainer).append($descriptionDiv);
+          $innerContainer.append($div).append($button);
+          $div.append($author).append($category);
+          $descriptionDiv.append($description);
+          $imageDiv.append($image);
+          // append the created elements to the $card
+          $card.append($imageDiv).append($outerContainer);
+          // append the card to the page
+          $("#information").append($card);
+        };
+      },
+      () => {
+        // if something goes wrong:
+        console.log("Houston, we have a problem.");
+      }
+    );
 
   };
 
